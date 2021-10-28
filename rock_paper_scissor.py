4a29969bd0416e50ace31139b11c3d7e07fc5d49
Shefaul Karim <shefaul2u@gmail.com>
2021-10-28 10:47:56 +0100

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,21 +3,21 @@ import random
 score_player1 = 0
 score_player2 = 0
 
-while score_player1 <= 2 and score_player2 <= 2:	
+while score_player1 <= 2 and score_player2 <= 2:	#you get to play until one of the player wins three times
 	print("************************\n************************\n************************")
 	
-	print(f"Your score = {score_player1}\nComputer Score = {score_player2}")
+	print(f"Your score = {score_player1}\nComputer Score = {score_player2}") #prints the scores
 
 	print("")
 
 	print("Here we go again!!!!!")
 
-	player1 = input("Please enter your move: ")
-	player1 = player1.lower()
+	player1 = input("Please enter your move: ")    #player playes his/her move
+	player1 = player1.lower()     #turns the player move to all lower case
 	player2 = None
 
-	ran = random.randint(0, 2)
-	
+	ran = random.randint(0, 2)     #we generate a random number(0-2) for the computer to play his move
+				       #here 0 is rock, 1 is paper and 2 is scissor
 	if ran == 0:
 		player2 = 'rock'
 		print("Computer plays "+ player2)
@@ -28,8 +28,8 @@ while score_player1 <= 2 and score_player2 <= 2:
 		player2 = 'scissor'
 		print("Computer plays "+ player2)
 
-	print("")
-
+	print("")    
+	#this is where we check who wins or loses and if it's a tie or not
 	if player1 == 'rock':
 		if player2 == 'scissor':
 			print("You win!!!!!!")
@@ -59,7 +59,7 @@ while score_player1 <= 2 and score_player2 <= 2:
 			print("It's a tie")		
 	else: 
 		print("Please enter a valid move")
-
+#shows who wins the game
 if score_player2 == 3:
 	print("Oops! Sorry, Computer wins")
 else:
